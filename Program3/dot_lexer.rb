52bafdabbef0e6518aa7733ec004896ff472d459
Trevor Keegan <keegant@uwplatt.edu>
2020-11-06 18:59:15 -0600

    done :)

--- Program3/dot_lexer.rb
+++ Program3/dot_lexer.rb
@@ -1,4 +1,10 @@
 require_relative 'token'
+
+# Author: Trevor Keegan
+# Date: 11/6/2020
+# The dot lexer will serve as a lexer for the dot language, Given an input of a file written in dot language, expect an
+# output as a list of categorized lexical units.
+
 class DotLexer
   @@regs = [ #https://regex101.com/ used heavily for this section for testing.
       /^[a-zA-Z0-9]*$/,          #ID => 1
@@ -17,18 +23,25 @@ class DotLexer
       /(\s)/                     #WS => 14
   ]
 
+
+  # Initialize the DotLexer class,
+  # Read in all text from the stdin, parse the text and make a list of tokens.
+  # Add an end of file token after finished reading the input.
+  # Initialize the iterator value to zero
   def initialize
     @tokens = make_tokens(get_all_in_from_stdin)
-    @tokens.push(Token.new('', -2))
+    @tokens.push(Token.new('', Token::EOF))
     @cur_token = 0
   end
 
+  # Get the next token that has been parsed.
   def next_token
     token = @tokens[@cur_token]
     @cur_token += 1
     token
   end
 
+  # Read all text available in stdin and join it together into s single string
   # @return [String] with all text available in console, joined at line break
   def get_all_in_from_stdin
     ins = []
@@ -40,10 +53,14 @@ class DotLexer
     ins.join()
   end
 
+  # Take in a string of text, split it into a set of tokens and non-tokens using the union of the lower 12 regex
+  # expressions in @@regs. This takes advantage of the decreasing match scope with each index in the list. After
+  # spitting the text into possible tokens, check each token for invalid characters, and build the token object.
+  # @param [String] all_text -> full input from stdin to be split and categorized into tokens
   def make_tokens(all_text)
     list_of_token_texts = []
     tokens = []
-    unwanted_characters = /([\s+,\t+,\n+,\r+])/
+    unwanted_characters = /([\s+,\t+,\n+,\r+])/ # Characters that should be removed after splitting
     regexp_for_split = Regexp.union(@@regs[2,13])
     all_text.split(regexp_for_split).each do |text_segment|
       possible_token = text_segment.gsub(unwanted_characters, '')
@@ -57,6 +74,9 @@ class DotLexer
     tokens
   end
 
+  # Build the token object if the text is a valid token, otherwire defer to handle_illegal_characters if something
+  # is wrong with the text. return a list of tokens either way. Not uncommon to receive a list including a single token
+  # but simpler to minimize the difference in output under varying conditions.
   def make_token(token_text)
     if is_token(token_text)
       [Token.new(token_text, get_token_type(token_text))]
@@ -65,10 +85,10 @@ class DotLexer
     end
   end
 
+  # Recursively get all valid tokens and invalid tokens in the token text.
   # @param [String] token_text, text from the input that has not matched any categorization
   # @return [List<Token>] - All invalid and valid tokens available in the provided TOKEN_TEXT
   def handle_illegal_characters(token_text)
-    
     return [] if token_text.length == 0
 
     return [Token.new(token_text, get_token_type(token_text))] if is_token(token_text)
@@ -84,7 +104,7 @@ class DotLexer
         tokens.push(valid_token)
       end
       invalid_text = token_text[index_under_evaluation - 1]
-      invalid_token = Token.new(invalid_text, -1)
+      invalid_token = Token.new(invalid_text, Token::INVALID)
       tokens.push(invalid_token)
       remaining_tokens = handle_illegal_characters(token_text[index_under_evaluation..-1])
       return tokens.concat(remaining_tokens)
@@ -102,6 +122,7 @@ class DotLexer
     false
   end
 
+  # get the type of token that can categorize the input text, return false if text is not a token.
   def get_token_type(text)
     (0...@@regs.length - 1 ).reverse_each do |i|
       regex_pattern = @@regs[i]
--- Program3/token.rb
+++ Program3/token.rb
@@ -1,26 +1,28 @@
+# Author: Trevor Keegan
+# Date: 11/6/2020
+# The Token is a pair of text and a category identifier 
 
 class Token
+  attr_accessor :type
 
-  attr_accessor :type, :text
-    @@EOF = -2
-    @@INVALID = -1
+  EOF = -2
+  INVALID = -1
 
-    def initialize (text, id)
-        super()
-        @text = text
-        @type = id
-    end
-
-  def  == (other)
-    @text == other.text and @type == other.type
+  # build the Token object
+  def initialize (text, id)
+      super()
+      @text = text
+      @type = id
   end
 
-    def to_s()
-        if @type != -1
-            return "[#{@text}:#{@type}]"
-        else
-            return "illegal char: #{@text}"
-        end
-    end
+  # Serialize the token to a string, if the token is valid, thus type != -1, expect ['text':'type']. 
+  # Otherwise expect illegal char: #{@type}
+  def to_s
+    if @type != INVALID
+          "[#{@text}:#{@type}]"
+      else
+          "illegal char: #{@text}"
+      end
+  end
 
 end
