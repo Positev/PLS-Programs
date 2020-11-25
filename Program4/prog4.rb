07c9746ce0f6860dc863561da08d1e12bbfa345a
Trevor Keegan <tkgn15@live.com>
2020-11-25 14:55:41 -0600

    adding program 4

---
+++ Program4/prog4.rb
@@ -0,0 +1,16 @@
+# This is the main class for program4, which was provided by the instructor.
+# Author:: Joshua Yue
+
+require_relative "dot_lexer"
+require_relative "dot_parser"
+
+# Uncomment this line for debugging.
+#$stdin.reopen("Prog4.in")
+
+lexer = DotLexer.new
+
+parser = DotParser.new(lexer)
+
+parser.graph()
+
+
