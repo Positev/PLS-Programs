fc8b8d4dbc62af2360e0235815d8c7679eb3105e
Trevor Keegan <tkgn15@live.com>
2020-11-30 15:44:19 -0600

    Revert "commit for revert test"
    This reverts commit aea84ac031aa7c723fae512f048553b5ba076fcb.

--- Program4/dot_parser.rb
+++ Program4/dot_parser.rb
@@ -3,10 +3,7 @@ require_relative 'token'
 
 $log = []
 
-def log (str)
-  puts str
-  $log.push(str.chomp)
-end
+
 class DotParser
   def clear_log()
     $log = []
@@ -23,15 +20,21 @@ class DotParser
     @restore_point = 0
   end
 
-
+  def log (str)
+    puts str
+    $log.push(str.chomp)
+  end
 
   #graph -> ('digraph' | 'DIGRAPH') [id] '{' stmt_list '}'
 
   def graph
     def parse
       accept Token::DIGRAPH
-      allowed_to_fail do
+      begin
+        drop_breadcrumb
         id
+      rescue SyntaxError
+        consume_breadcrumb
       end
       if should_accept(Token::LCURLY)
         accept Token::LCURLY
@@ -49,18 +52,23 @@ class DotParser
   def stmt_list
     def parse
       loop do
-        return unless allowed_to_fail do
+        begin
+          drop_breadcrumb
           stmt
           if should_accept Token::SEMI
             accept Token::SEMI
           end
 
           if should_accept Token::RCURLY
-            return true
-            end
+            return
+          end
+        rescue SyntaxError
+          consume_breadcrumb
+          return
+        end
+
       end
-      end
-      end
+    end
 
     log "Start recognizing a cluster"
     output = parse
@@ -70,22 +78,29 @@ class DotParser
   #stmt -> edge_stmt | id '=' id | subgraph
   def stmt
     def parse
-
-      return unless allowed_to_fail do
+      begin
+        drop_breadcrumb
         edge_stmt
-        true
+        return
+      rescue SyntaxError
+        consume_breadcrumb
       end
 
-      return unless allowed_to_fail do
+      begin
+        drop_breadcrumb
         property
-        true
+        return
+      rescue SyntaxError
+        consume_breadcrumb
       end
 
-      return unless allowed_to_fail do
+      begin
+        drop_breadcrumb
         subgraph
-        true
+        return
+      rescue SyntaxError
+        consume_breadcrumb
       end
-
 
       error('')
     end
@@ -237,7 +252,7 @@ class DotParser
     if not either
       allowed_to_fail do
         subgraph
-
+        
       end
       begin
         drop_breadcrumb
@@ -249,16 +264,14 @@ class DotParser
     end
   end
 
-  #return false if fails, True if does not fail
   def allowed_to_fail
     begin
       drop_breadcrumb
       yield
     rescue  SyntaxError
       consume_breadcrumb
-      false
     end
-    true
+
   end
 
   def error(msg)
